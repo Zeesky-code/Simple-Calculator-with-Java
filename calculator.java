f1d7ca8d3bd5ca278aea69af08a9dbb11880ddfb
Zainab Lawal <zainablaw1012@gmail.com>
2022-01-03 14:51:49 +0300

    created subtraction method

--- calculator.java
+++ calculator.java
@@ -3,6 +3,10 @@ public class calculator {
         int sum = num1 + num2;
         return sum;
     }
+    static int subtraction(int num1, int num2) {
+        int difference = num1 - num2;
+        return difference;
+    }
     
   
     public static void main(String[] args) {
