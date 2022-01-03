7852703bf96408262fe670fc9a3f539b82f869a6
Zainab Lawal <zainablaw1012@gmail.com>
2022-01-03 15:48:25 +0300

    Added user input for addition method

--- calculator.java
+++ calculator.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class calculator {
     static int addition(int num1, int num2) {
         int sum = num1 + num2;
@@ -10,6 +11,18 @@ public class calculator {
     
   
     public static void main(String[] args) {
-        System.out.println(addition(2,4));
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the Calculator");
+        System.out.println("*************************");
+        System.out.println("What operation would you like to perform?" + '\n' + "Press 1 for addition or 2 for subtraction");
+        int choice = input.nextInt();
+        if (choice == 1){
+            System.out.println("Enter the first number:");
+            int first = input.nextInt();
+            System.out.println("Enter the second number:");
+            int second = input.nextInt();
+            //calling the addition method
+            System.out.println("The sum of the numbers is "+ addition(first,second));
+        }
     }
 }
